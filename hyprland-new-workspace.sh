dab92d61dc279c96cc2962bb219f75438dd96d45
kuokuo123 <31716434+kuokuo123@users.noreply.github.com>
2024-03-03 22:06:57 +0800

    streamline codes

--- hyprland-new-workspace.sh
+++ hyprland-new-workspace.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
-fIsnotempty=$(hyprctl workspaces | grep -A 2 "monitor $(hyprctl monitors -j | jq -r 'map(select(.focused == true)) | .[] | .name')" | grep "windows: 0")
+focusedwsisempty=$(hyprctl workspaces | grep -A 2 "monitor $(hyprctl monitors -j | jq -r 'map(select(.focused == true)) | .[] | .name')" | grep "windows: 0")
 
-if [[ -z $fIsnotempty ]]; then
-    numbers=($(hyprctl workspaces | grep "ID [0-99]" | cut -d " " -f 3 | sort -h | tr '\n' ' '))
+if [[ -z $focusedwsisempty ]]; then
+
+    openedwslist=($(hyprctl workspaces | grep "ID [0-99]" | cut -d " " -f 3 | sort -h | tr '\n' ' '))
     calculated_num=0
 
-    for number in "${numbers[@]}"; do
-	if (( calculated_num != 0 && number != calculated_num + 1 )); then
+    for i in "${openedwslist[@]}"; do
+	if (( calculated_num != 0 && i != calculated_num + 1 )); then
 	    if [[ $1 = "--move" ]]; then 
 		hyprctl dispatch movetoworkspace $((calculated_num + 1))
-	    else
-		hyprctl dispatch workspace $((calculated_num + 1))
+		else
+		    hyprctl dispatch workspace $((calculated_num + 1))
 	    fi
 	    exit 0
 	fi
-	calculated_num=$number
+	calculated_num=$i
     done  
 
     if [[ $1 = "--move" ]]; then 
 	hyprctl dispatch movetoworkspace $((calculated_num + 1))
-    else
-	hyprctl dispatch workspace $((calculated_num + 1))
+	else
+	    hyprctl dispatch workspace $((calculated_num + 1))
     fi
 
 else
-    echo "Alreading focusing an empty workspace. aborting.."
+    echo "Already focusing an empty workspace..."
 fi
-
